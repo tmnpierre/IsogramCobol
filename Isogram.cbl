01775f1cdbd5d53794765861b06067dfccd7bb17
tmnpierre <tomyn.pierre@gmail.com>
2024-04-23 14:35:19 +0200

    initial commit

--- Isogram.cbl
+++ Isogram.cbl
@@ -25,38 +25,49 @@
        01  ISISOGRAM PIC X(03) VALUE 'YES'. 
       *    Valeur ASCII du caractère actuel
        01  CHAR-VAL PIC 9(03) COMP. 
+      *    Pour stocker la réponse de l'utilisateur (Oui/Non)
+       01  ANS PIC X(01).   
 
        PROCEDURE DIVISION.
 
-           DISPLAY "Enter a phrase to check if it's an isogram: "
-           ACCEPT INPUTPHRASE
+           PERFORM UNTIL ANS = 'N'
+               DISPLAY "Enter a phrase to check if it's an isogram: "
+               ACCEPT INPUTPHRASE
 
       *    Itère à travers chaque caractère de la phrase jusqu'à ce que 
       *    la fin de la phrase soit atteinte ou jusqu'à ce qu'il soit 
       *    déterminé que la phrase n'est pas un isogramme
-           PERFORM VARYING CHARPOSITION FROM 1 BY 1 UNTIL 
-           CHARPOSITION > LENGTH OF INPUTPHRASE OR ISISOGRAM = 'NO '
+               PERFORM VARYING CHARPOSITION FROM 1 BY 1 UNTIL 
+               CHARPOSITION > LENGTH OF INPUTPHRASE OR ISISOGRAM = 'NO'
 
       *    Convertit le caractère actuel en majuscule et détermine 
       *    sa valeur ASCII
-           MOVE FUNCTION ORD(FUNCTION UPPER-CASE(
-                          INPUTPHRASE(CHARPOSITION:1))) TO CHAR-VAL
+                   MOVE FUNCTION ORD(FUNCTION UPPER-CASE(
+                              INPUTPHRASE(CHARPOSITION:1))) TO CHAR-VAL
 
       *    Vérifie si le caractère est une lettre de l'alphabet
-               IF CHAR-VAL >= 65 AND CHAR-VAL <= 90 THEN
+                   IF CHAR-VAL >= 65 AND CHAR-VAL <= 90 THEN
       *    Calcule l'indice dans le tableau pour le caractère
-                   COMPUTE LETTERCOUNT = CHAR-VAL - 64  
+                       COMPUTE LETTERCOUNT = CHAR-VAL - 64  
       *    Vérifie si le caractère a déjà été rencontré
-                   IF ALPHABETOCCURS (LETTERCOUNT) > 0 THEN
+                       IF ALPHABETOCCURS (LETTERCOUNT) > 0 THEN
       *    Si oui, la phrase n'est pas un isogramme
-                       MOVE 'NO ' TO ISISOGRAM              
-                   ELSE
+                           MOVE 'NO ' TO ISISOGRAM              
+                       ELSE
       *    Sinon, met à jour le tableau des occurrences
-                       ADD 1 TO ALPHABETOCCURS (LETTERCOUNT)   
+                           ADD 1 TO ALPHABETOCCURS (LETTERCOUNT)   
+                       END-IF
                    END-IF
-               END-IF
-           END-PERFORM
+               END-PERFORM
 
       *    Affiche le résultat
-           DISPLAY "Is the phrase an isogram? " ISISOGRAM
+               DISPLAY "Is the phrase an isogram? " ISISOGRAM
+
+               DISPLAY "Do you want to check another phrase? (Y/N): "
+               WITH NO ADVANCING
+               ACCEPT ANS
+               MOVE FUNCTION UPPER-CASE(ANS) TO ANS
+           END-PERFORM
+
            STOP RUN.
+
